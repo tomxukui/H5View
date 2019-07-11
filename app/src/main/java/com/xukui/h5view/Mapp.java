3ec397e544e0cd9af37deb5e87130b87a3f308b6
xukui <xukui@wondersgroup.com>
2019-07-11 09:22:49 +0800

    init

--- app/src/main/java/com/xukui/h5view/Mapp.java
+++ app/src/main/java/com/xukui/h5view/Mapp.java
@@ -1,17 +1,26 @@
 package com.xukui.h5view;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.xukui.library.h5view.H5Client;
 
 public class Mapp extends Application {
 
+    private static final String TAG = "Mapp";
+
     @Override
     public void onCreate() {
         super.onCreate();
-        H5Client.getInstance()
-                .init(this)
-                .setupX5Core(true, null);
+        //如果需要使用x5内核, 则需要执行下面代码, 否则直接用，无需配置
+        H5Client.setupX5Core(this, true, (isSuccess, msg) -> {
+            if (isSuccess) {
+                Log.i(TAG, msg);
+
+            } else {
+                Log.e(TAG, msg);
+            }
+        });
     }
 
 }
--- library/src/main/java/com/xukui/library/h5view/H5Client.java
+++ library/src/main/java/com/xukui/library/h5view/H5Client.java
@@ -1,6 +1,5 @@
 package com.xukui.library.h5view;
 
-import android.app.Application;
 import android.content.Context;
 import android.support.annotation.Nullable;
 
@@ -12,37 +11,18 @@ import java.util.HashMap;
 
 public class H5Client {
 
-    private Context mContext;
-
     private H5Client() {
-    }
-
-    public static H5Client getInstance() {
-        return SingletonHolder.INSTANCE;
-    }
-
-    private static class SingletonHolder {
-        private static final H5Client INSTANCE = new H5Client();
-    }
-
-    public H5Client init(Application application) {
-        mContext = application.getApplicationContext();
-        return this;
-    }
-
-    public Context getContext() {
-        return mContext;
     }
 
     /**
      * 组建X5浏览器内核
      */
-    public void setupX5Core(boolean downloadWithoutWifi, @Nullable final OnX5InitListener listener) {
+    public static void setupX5Core(Context context, boolean downloadWithoutWifi, @Nullable final OnX5InitListener listener) {
         HashMap<String, Object> map = new HashMap<>();
         map.put(TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER, true);
         QbSdk.initTbsSettings(map);
 
-        QbSdk.initX5Environment(mContext, new QbSdk.PreInitCallback() {
+        QbSdk.initX5Environment(context, new QbSdk.PreInitCallback() {
 
             @Override
             public void onCoreInitFinished() {
